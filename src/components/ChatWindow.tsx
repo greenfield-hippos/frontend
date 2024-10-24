0952d59fd4e6cc196fe1c29c71f16676a9052815
vicentbe10 <vicentbe10@gmail.com>
2024-10-25 01:25:03 +0900

    some additional dummy components for the ChatWindow

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,15 +1,13 @@
+import MessageList from "./MessageList";
+import SendMessage from "./SendMessage";
+
 const ChatWindow = () => {
 
     return (
         <>
             <div className="chat-window">
-                <p>Messages</p>
-                <p>Messages</p>
-                <p>Messages</p>
-                <p>Messages</p>
-                <p>Messages</p>
-                <textarea>This is the send message</textarea>
-                <button>Send</button>
+                <MessageList />
+                <SendMessage />
             </div>
         </>
     )
---
+++ src/components/MessageList.tsx
@@ -0,0 +1,18 @@
+const MessageList = () => {
+
+    return (
+        <>
+            <div className="message-list">
+                <MessageList />
+                <p>Messages</p>
+                <p>Messages</p>
+                <p>Messages</p>
+                <p>Messages</p>
+                <p>Messages</p>
+            </div>
+        </>
+    )
+
+}
+
+export default MessageList;
