7a5a4b9b3674946d214a350311a3408b16d68afc
hono1030 <honoka.n1030@gmail.com>
2024-10-29 10:50:17 +0900

    feat/switch-ti-fav-list-view

--- src/components/SidebarContentList.tsx
+++ src/components/SidebarContentList.tsx
@@ -2,25 +2,27 @@ import ConversationList from "./ConversationList";
 import { Conversation } from "../types";
 
 interface SidebarContentListProps {
-    conversations: Conversation[];
-    onSelectConversation: (id: string | null) => void;
+  conversations: Conversation[];
+  onSelectConversation: (id: string | null) => void;
+  fetchFavoriteData: () => void;
 }
 
 const SidebarContentList: React.FC<SidebarContentListProps> = ({
-    conversations,
-    onSelectConversation,
+  conversations,
+  onSelectConversation,
+  fetchFavoriteData,
 }) => {
-
-    return (
-        <>  
-            <div className="sidebar-content">
-                <ConversationList 
-                    conversations={conversations}
-                    onSelectConversation={onSelectConversation}
-                />              
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="sidebar-content">
+        <ConversationList
+          conversations={conversations}
+          onSelectConversation={onSelectConversation}
+          fetchFavoriteData={fetchFavoriteData}
+        />
+      </div>
+    </>
+  );
+};
 
 export default SidebarContentList;
